49c58a53c91e0a5d5905bb6622028b976aa73b16
Jorge <jorgefprietol@gmail.com>
2022-08-24 12:28:28 -0500

    refrescando las configuraciones desde el config server. centralizando v2

--- src/main/java/com/microservicio/jfpl/producto/productomicroservicio/controller/CategoryController.java
+++ src/main/java/com/microservicio/jfpl/producto/productomicroservicio/controller/CategoryController.java
@@ -3,6 +3,8 @@ package com.microservicio.jfpl.producto.productomicroservicio.controller;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/categories")
 @Getter
 @Setter
+@RefreshScope
 public class CategoryController {
 
     @Value("${app.testProp}")
     private  String testProp;
 
+    @GetMapping("test-prop")
     public String getTestProp(){
 
         return this.testProp;
