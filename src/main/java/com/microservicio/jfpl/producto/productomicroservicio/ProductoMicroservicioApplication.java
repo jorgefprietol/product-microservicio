4f826c1a611c1074e86ff472e5bcde4672681ada
Jorge <jorgefprietol@gmail.com>
2022-08-23 20:43:14 -0500

    configdb-entity-repository-controller

---
+++ src/main/java/com/microservicio/jfpl/producto/productomicroservicio/ProductoMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.jfpl.producto.productomicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoMicroservicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservicio/jfpl/producto/productomicroservicio/ProductoMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.jfpl.producto.productomicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
